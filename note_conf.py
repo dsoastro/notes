ab3f42ab47fa30df5991a3f8f7a8fb8651598e70
leonid <vasiliev.leonid@gmail.com>
2023-04-29 09:51:42 +0300

    make PATH variable initialisation more intuitive

--- note_conf.py
+++ note_conf.py
@@ -1,4 +1,4 @@
-PATH = "/home/user/notes/"             # Your path to root note folder here
+PATH = "/home/user/noterootfolder/"    # Your path to root note folder here
 HISTORY_LOCATION = "/home/user/"       # Your path to place to save history here
 EDITOR = "nano"                        # Your preferred CLI editor
 PHONE = False                          # Set to true when you run it on phone with termux
